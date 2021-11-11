b69d459588b93bcd12507f1ed0178553bcd2cc77
LENOVO <LENOVO@DESKTOP-FFTBH50>
2021-11-11 12:36:28 +0530

    user 1 First Commit

---
+++ GitHub.practice/src/test/java/github_praticeScript/Demo1.java
@@ -0,0 +1,12 @@
+package github_praticeScript;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+     @Test
+     public void demo()
+     {
+    	 System.out.println("Hi this is user 1 This is my first commit");
+     }
+}
